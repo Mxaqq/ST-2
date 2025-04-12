271602a74cc198855bfc37b6bd0fab71e553af98
Maksim Golovkin <Mxaqq>
2025-04-12 06:45:25 +0300

    Max

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,19 @@
 // Copyright 2022 UNN-CS
-#ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
 
+class Circle {
+private:
+    double radius = 0;
+    double ference = 0;
+    double area = 0;
+public:
+    void setRadius(double radius);
+    void setFerence(double ference);
+    void setArea(double area);
+    double getRadius();
+    double getFerence();
+    double getArea();
+};
 #endif  // INCLUDE_CIRCLE_H_
