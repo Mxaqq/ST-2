563dfdb2a038d81df1856d83cc33c36f8f3669cb
Maksim Golovkin <Mxaqq>
2025-06-02 13:51:15 +0300

    TaskFix

--- include/circle.h
+++ include/circle.h
@@ -3,26 +3,25 @@
 #define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
-
 class Circle {
 private:
-    double radius;
-    double ference;
-    double area;
+	double radius;
+	double ference;
+	double area;
 
-    void updateFromRadius();
-    void updateFromFerence();
-    void updateFromArea();
+	void updateFromRadius();
+	void updateFromFerence();
+	void updateFromArea();
 
 public:
-    explicit Circle(double r = 0);
+	explicit Circle(double r = 0);
 
-    void setRadius(double radius);
-    void setFerence(double ference);
-    void setArea(double area);
+	void setRadius(double radius);
+	void setFerence(double ference);
+	void setArea(double area);
 
-    double getRadius() const;
-    double getFerence() const;
-    double getArea() const;
+	double getRadius() const;
+	double getFerence() const;
+	double getArea() const;
 };
 #endif  // INCLUDE_CIRCLE_H_
