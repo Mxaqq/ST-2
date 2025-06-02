00c1668b3e3bd1ef810db29b1f93445343930742
Maksim Golovkin <Mxaqq>
2025-06-02 15:53:21 +0300

    fix

--- include/tasks.h
+++ include/tasks.h
@@ -1,9 +1,28 @@
 // Copyright 2022 UNN-CS
-#ifndef INCLUDE_TASKS_H_
-#define INCLUDE_TASKS_H_
-#include <cstdint>
+// Copyright 2022 UNN-CS
+#ifndef TASKS_H_
+#define TASKS_H_
 
-double calculateEarthRopeGap();
-double calculatePoolConstructionCost();
+#include "circle.h"
 
-#endif  // INCLUDE_TASKS_H_
+namespace {const double EARTH_RADIUS = 6378100.0;const double POOL_RADIUS = 3.0;const double PATH_WIDTH = 1.0;const double CONCRETE_PRICE = 1000.0;const double FENCE_PRICE = 2000.0;}
+
+// Вычисляет зазор между землёй и верёвкой (задача про Землю и верёвку)
+double Earth() {
+    Circle earth(EARTH_RADIUS);
+    earth.setFerence(earth.getFerence() + 1.0);
+    return earth.getRadius() - EARTH_RADIUS;
+}
+
+// Вычисляет стоимость строительства бассейна с дорожкой и ограждением
+double track_cost() {
+    Circle pool(POOL_RADIUS);
+    Circle poolWithPath(POOL_RADIUS + PATH_WIDTH);
+
+    double pathArea = poolWithPath.getArea() - pool.getArea();
+    double fenceLength = poolWithPath.getFerence();
+
+    return (pathArea * CONCRETE_PRICE) + (fenceLength * FENCE_PRICE);
+}
+
+#endif  // TASKS_H_
--- src/tasks.cpp
+++ src/tasks.cpp
@@ -1,23 +1,13 @@
 // Copyright 2022 UNN-CS
 #include "tasks.h"
-#include "circle.h"
-#include <cmath>
 
-namespace {
-    const double EARTH_RADIUS = 6378100.0;
-    const double POOL_RADIUS = 3.0;
-    const double PATH_WIDTH = 1.0;
-    const double CONCRETE_PRICE = 1000.0;
-    const double FENCE_PRICE = 2000.0;
-}
-
-double calculateEarthRopeGap() {
+double Earth() {
     Circle earth(EARTH_RADIUS);
     earth.setFerence(earth.getFerence() + 1.0);
     return earth.getRadius() - EARTH_RADIUS;
 }
 
-double calculatePoolConstructionCost() {
+double track_cost() {
     Circle pool(POOL_RADIUS);
     Circle poolWithPath(POOL_RADIUS + PATH_WIDTH);
 
